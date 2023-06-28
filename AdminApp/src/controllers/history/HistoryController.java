ef7ea2a82c4281cc59f30a7e2565fa0640bfc73e
Michael Shuster <michaelsu@mta.ac.il>
2023-06-28 11:04:33 +0300

    started working on admin app

---
+++ AdminApp/src/controllers/history/HistoryController.java
@@ -0,0 +1,261 @@
+package controllers.history;
+
+import controllers.AppController;
+import dto.FlowExecutionDTO;
+import elementlogic.ElementLogic;
+import enginemanager.EngineApi;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class HistoryController {
+    @FXML
+    private StackPane stackTableView;
+
+    @FXML
+    private ChoiceBox<String> stateFilterView;
+
+    @FXML
+    private VBox elementDetailsView;
+
+    @FXML
+    private VBox elementChoiceView;
+
+    @FXML
+    private Button reRunButton;
+
+    @FXML
+    private Button continuationButton;
+
+    private AppController appController;
+
+    private EngineApi engine;
+
+
+    private TableView<FlowExecutionDTO> historyTableView;
+
+    private ObservableList<FlowExecutionDTO> tableData;
+
+    private TableColumn<FlowExecutionDTO,String> flowNameColumnView;
+
+    private TableColumn<FlowExecutionDTO,String> activationTimeColumnView;
+
+    private TableColumn<FlowExecutionDTO,String> flowStateColumnView;
+
+    private ElementLogic elementLogic;
+
+
+
+
+    @FXML
+    public void initialize() {
+
+        historyTableView=new TableView<>();
+        historyTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        flowNameColumnView=new TableColumn<>("flow name");
+        activationTimeColumnView=new TableColumn<>("activation time");
+        flowStateColumnView =new TableColumn<>("state after run");
+
+        flowNameColumnView.setCellValueFactory(new PropertyValueFactory<>("name"));
+        flowStateColumnView.setCellValueFactory(new PropertyValueFactory<>("stateAfterRun"));
+        activationTimeColumnView.setCellValueFactory(new PropertyValueFactory<>("activationTime"));
+
+        historyTableView.getColumns().addAll(flowNameColumnView,activationTimeColumnView, flowStateColumnView);
+        historyTableView.getColumns().forEach(column -> column.setMinWidth(200));
+        historyTableView.setOnMouseClicked(e-> HistoryTableRowClick(new ActionEvent()));
+        historyTableView.setEditable(false);
+        stackTableView.getChildren().add(historyTableView);
+
+        stateFilterView.getItems().addAll("ALL", "SUCCESS", "WARNING", "FAILURE");
+        stateFilterView.setValue("ALL");
+        stateFilterView.setOnAction(event -> filterTable());
+
+        tableData = FXCollections.observableArrayList();
+        historyTableView.setItems(tableData);
+
+        reRunButton.setDisable(true);
+        continuationButton.setDisable(true);
+
+
+        historyTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                reRunButton.setDisable(false);
+                checkIfContinuationsAvailable(historyTableView.getSelectionModel().getSelectedItem());
+            } else {
+                reRunButton.setDisable(true);
+                continuationButton.setDisable(true);
+            }
+        });
+
+
+    }
+
+    private void checkIfContinuationsAvailable(FlowExecutionDTO selectedItem) {
+        if (engine.getContinuationMenuDTOByName(selectedItem.getName()) != null)
+            continuationButton.setDisable(false);
+        else
+            continuationButton.setDisable(true);
+    }
+
+
+    public void setAppController(AppController appController) {
+        this.appController = appController;
+    }
+
+    public void setStage(Stage stage)
+    {
+        elementLogic=new ElementLogic(elementChoiceView,elementDetailsView,stage);
+    }
+
+    public void setEngine(EngineApi engine) {
+        this.engine = engine;
+    }
+
+    @FXML
+    void reRunFlow(ActionEvent event) {
+        if(!historyTableView.getSelectionModel().isEmpty()) {
+            FlowExecutionDTO flowExecutionDTO = historyTableView.getSelectionModel().getSelectedItem();
+            engine.reUseInputsData(flowExecutionDTO);
+            appController.streamFlow(flowExecutionDTO.getName());
+        }
+
+    }
+
+    @FXML
+    private void HistoryTableRowClick(ActionEvent event) {
+
+        if(!historyTableView.getSelectionModel().isEmpty()) {
+            FlowExecutionDTO flowExecutionDTO = historyTableView.getSelectionModel().getSelectedItem();
+            elementLogic.setElementDetailsView(flowExecutionDTO);
+        }
+    }
+
+
+    public void addRow(FlowExecutionDTO flowExecutionDTO)
+    {
+        tableData.add(0,flowExecutionDTO);
+    }
+
+    public void filterTable()
+    {
+        String choice = stateFilterView.getValue();
+        FilteredList<FlowExecutionDTO> filteredData = new FilteredList<>(tableData);
+        switch(choice) {
+            case "ALL":
+                historyTableView.setItems(tableData);
+                break;
+            case "SUCCESS":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("SUCCESS");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+            case "FAILURE":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("FAILURE");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+            case "WARNING":
+                filteredData.setPredicate(item -> {
+                    return item.getStateAfterRun().equals("WARNING");
+                });
+                historyTableView.setItems(filteredData);
+                break;
+
+        }
+        historyTableView.refresh();
+    }
+
+    public void clearTab()
+    {
+        elementLogic.clear();
+        tableData.clear();
+        historyTableView.getItems().clear();
+        stateFilterView.setValue("ALL");
+    }
+
+
+    @FXML
+    void showFlowInfo(MouseEvent event) {
+        elementLogic.updateFlowInfoView();
+    }
+
+    @FXML
+    void openContinuationPopUp(ActionEvent event) {
+        FlowExecutionDTO flowExecutionDTO = historyTableView.getSelectionModel().getSelectedItem();
+        TextInputDialog inputDialog =getNewTextInputDialog();
+        Optional<String> result = Optional.empty();
+        ChoiceBox<String> continuationChoice = new ChoiceBox<>();
+        continuationChoice.getItems().addAll(engine.getContinuationMenuDTOByName(flowExecutionDTO.getName()).getTargetFlows());
+        continuationChoice.setStyle("-fx-pref-width: 200px;");
+
+        HBox hbox = new HBox(10, new Label("Available Continuations:"), continuationChoice);
+        hbox.setAlignment(Pos.CENTER);
+        inputDialog.getDialogPane().setContent(hbox);
+
+        inputDialog.setResultConverter(dialogButton -> {
+            if (dialogButton == ButtonType.OK) {
+                String selectedOption = continuationChoice.getValue();
+                return selectedOption;
+            }
+            return null;
+        });
+
+        Button submitButton=(Button) inputDialog.getDialogPane().lookupButton(ButtonType.OK);
+
+        continuationChoice.valueProperty().addListener((observable, oldValue, newValue) -> {
+            submitButton.setDisable(newValue == null);
+        });
+
+        result = inputDialog.showAndWait();
+        if(result.isPresent())
+        {
+            String targetName= result.get();
+            continueToFlow(targetName, flowExecutionDTO.getId());
+        }
+    }
+
+    private TextInputDialog getNewTextInputDialog()
+    {
+        TextInputDialog inputDialog = new TextInputDialog();
+
+        inputDialog.setTitle("Choose continuation");
+        inputDialog.setHeaderText(null);
+        inputDialog.setGraphic(null);
+        inputDialog.getDialogPane().setPrefWidth(400);
+
+        Button submitButton=(Button) inputDialog.getDialogPane().lookupButton(ButtonType.OK);
+        submitButton.setText("Continue to flow");
+
+        submitButton.setDisable(true);
+
+        TextField textField = inputDialog.getEditor();
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            submitButton.setDisable(newValue.trim().isEmpty());
+        });
+
+        if(appController.getPrimaryStage().getScene().getStylesheets().size()!=0)
+            inputDialog.getDialogPane().getStylesheets().add(appController.getPrimaryStage().getScene().getStylesheets().get(0));
+        return  inputDialog;
+    }
+
+    void continueToFlow(String targetName, String id) {
+        engine.doContinuation(engine.getFlowExecution(id),targetName);
+        appController.streamFlow(targetName);
+    }
+
+}
---
+++ AdminApp/src/elementlogic/ElementLogic.java
@@ -0,0 +1,405 @@
+package elementlogic;
+
+import datadefinition.DataType;
+import datadefinition.Relation;
+import dto.*;
+import javafx.animation.FadeTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.List;
+import java.util.Map;
+
+public class ElementLogic {
+    private VBox elementChoiceView;
+    private VBox elementDetailsView;
+    private Stage primaryStage;
+    private FlowExecutionDTO flowExecutionDTO;
+
+    private TableView<StepExecutionDTO> stepsTableView;
+
+    private TableColumn<StepExecutionDTO,String> stepColumnView;
+
+    private TableColumn<StepExecutionDTO,String> stateColumnView;
+    boolean tableClicked;
+
+
+    public ElementLogic(VBox elementChoiceView, VBox elementDetailsView, Stage primaryStage) {
+        this.elementChoiceView = elementChoiceView;
+        this.elementDetailsView = elementDetailsView;
+        this.primaryStage=primaryStage;
+        this.tableClicked = false;
+
+        stepsTableView=new TableView<>();
+        stepColumnView=new TableColumn<>("step");
+        stateColumnView=new TableColumn<>("state");
+
+        stepColumnView.setCellValueFactory(new PropertyValueFactory<>("name"));
+        stateColumnView.setCellValueFactory(new PropertyValueFactory<>("stateAfterRun"));
+
+        stepsTableView.getColumns().addAll(stepColumnView,stateColumnView);
+        stepsTableView.setOnMouseClicked(e->rowClick(new ActionEvent()));
+        stepsTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        elementChoiceView.getChildren().add(stepsTableView);
+        VBox.setVgrow(stepsTableView, Priority.ALWAYS);
+    }
+
+    public void setTableOpacity(Double opacity) {
+        stepsTableView.setOpacity(opacity);
+    }
+
+    public void animateTable() {
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), stepsTableView);
+            fadeTransition.setFromValue(0.0);
+            fadeTransition.setToValue(1.0);
+            fadeTransition.play();
+    }
+
+
+
+
+    public void setElementDetailsView(FlowExecutionDTO flowExecutionDTO)
+    {
+        this.flowExecutionDTO=flowExecutionDTO;
+        ObservableList<StepExecutionDTO> items= FXCollections.observableArrayList();
+        items.addAll(flowExecutionDTO.getSteps());
+        stepsTableView.setItems(items);
+
+        if(flowExecutionDTO.getStateAfterRun()!=null)
+            updateFlowInfoView();
+
+    }
+
+
+
+    public void clear()
+    {
+        elementDetailsView.getChildren().clear();
+        stepsTableView.getItems().clear();
+        flowExecutionDTO=null;
+        tableClicked = false;
+    }
+
+    public boolean isTableClicked() {
+        return tableClicked;
+    }
+
+    @FXML
+    private void rowClick(ActionEvent event)
+    {
+        if(!stepsTableView.getSelectionModel().isEmpty()) {
+            elementDetailsView.getChildren().clear();
+            StepExecutionDTO stepExecutionDTO=stepsTableView.getSelectionModel().getSelectedItem();
+            StepExtensionDTO stepExtensionDTO =stepExecutionDTO.getStepExtensionDTO();
+            tableClicked = true;
+
+            addTitleLine("STEP'S DETAILS: ");
+            addKeyValueLine("Name: ",stepExecutionDTO.getName());
+            addKeyValueLine("Run Time: ",stepExecutionDTO.getRunTime()+ "ms");
+            addKeyValueLine("Finish state: ",stepExecutionDTO.getStateAfterRun());
+            addTitleLine("\nSTEP'S INPUT DATA:");
+            addStepInputsOrOutputsData(stepExtensionDTO.getInputs());
+            addTitleLine("\nSTEP'S OUTPUTS DATA:");
+            addStepInputsOrOutputsData(stepExtensionDTO.getOutputs());
+            addTitleLine("\nSTEP LOGS: ");
+            addStepLogs(stepExtensionDTO.getLogs());
+        }
+    }
+
+    private HBox getNewHbox()
+    {
+        HBox hBox =new HBox();
+        hBox.setAlignment(Pos.BASELINE_LEFT);
+        hBox.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        hBox.setPrefWidth(Region.USE_COMPUTED_SIZE);
+        return  hBox;
+    }
+
+    private void addKeyValueLine(String name, String value)
+    {
+        HBox hBox = getNewHbox();
+
+        Label key =new Label(name);
+        key.setFont(Font.font("System", FontWeight.BOLD,12));
+
+        Label data =new Label(value);
+        data.setAlignment(Pos.TOP_LEFT);
+
+        hBox.getChildren().add(key);
+        hBox.getChildren().add(data);
+
+        elementDetailsView.getChildren().add(hBox);
+    }
+
+
+    private void addKeyHyperLinkValueLine(String name, String value, Object data)
+    {
+        HBox hBox = getNewHbox();
+
+        Label label =new Label(name+": ");
+        label.setFont(Font.font("System",FontWeight.BOLD,12));
+
+        Hyperlink hyperlink=new Hyperlink(value);
+
+        switch (value)
+        {
+            case "Relation":
+                hyperlink.setOnMouseClicked(e->relationPopUp((Relation) data));
+                break;
+            case "List":
+                hyperlink.setOnMouseClicked(e->listPopUp((List<Object>) data));
+                break;
+        }
+
+
+
+        hBox.getChildren().add(label);
+        hBox.getChildren().add(hyperlink);
+
+        elementDetailsView.getChildren().add(hBox);
+    }
+
+    private void addKeyProgressIndicator(String name)
+    {
+        HBox hBox = getNewHbox();
+
+        Label key =new Label(name);
+        key.setFont(Font.font("System", FontWeight.BOLD,12));
+
+        hBox.setMaxHeight(14);
+
+        ProgressIndicator progressIndicator =new ProgressIndicator();
+        progressIndicator.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
+
+        hBox.getChildren().add(key);
+        hBox.getChildren().add(progressIndicator);
+
+        elementDetailsView.getChildren().add(hBox);
+    }
+
+
+
+    private void addTitleLine(String title)
+    {
+        HBox hBox= getNewHbox();
+        Label label = new Label(title);
+        label.setFont(Font.font("System",FontWeight.BOLD,14));
+        hBox.getChildren().add(label);
+        elementDetailsView.getChildren().add(hBox);
+    }
+
+
+    private void addStepInputsOrOutputsData(Map<DataDefintionDTO,Object> io)
+    {
+        String value="";
+        for(DataDefintionDTO dataDefintionDTO:io.keySet())
+        {
+            String name=dataDefintionDTO.getName();
+            String type=dataDefintionDTO.getType();
+            Object data=io.get(dataDefintionDTO);
+
+            if(data!=null) {
+                if(type.equals(DataType.RELATION.toString())||type.equals(DataType.LIST.toString()))
+                    addKeyHyperLinkValueLine(name,type,data);
+                else
+                    addKeyValueLine(name+": ",data.toString());
+            }
+            else
+                addKeyValueLine(name+": ","No Data Received");
+        }
+        if(io.keySet().size()==0)
+            addKeyValueLine("No outputs exist","");
+    }
+
+
+    private void showNewPopUp(Parent root)
+    {
+        final Stage stage = new Stage();
+        stage.initOwner(primaryStage);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        Scene scene = new Scene(root, 400, 300);
+        if(primaryStage.getScene().getStylesheets().size()!=0)
+            scene.getStylesheets().add(primaryStage.getScene().getStylesheets().get(0));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    private TableView<Map<String, String>> createTableView(Relation data)
+    {
+        TableView<Map<String, String>> tableView=new TableView<>();
+        List<String> columnNames=data.getColumnNames();
+
+        for(String columnName :columnNames)
+        {
+            TableColumn<Map<String, String>,String> columnView=new TableColumn<>(columnName);
+            columnView.setCellValueFactory(new MapValueFactory(columnName));
+            tableView.getColumns().add(columnView);
+        }
+
+        ObservableList<Map<String,String>> items= FXCollections.observableArrayList();
+        items.addAll(data.getRows());
+        tableView.setItems(items);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableView.setEditable(false);
+        return  tableView;
+    }
+
+    private void relationPopUp(Relation data) {
+
+        showNewPopUp(createTableView(data));
+    }
+
+    private void listPopUp(List<Object> data)
+    {
+        if(data.size()==0) {
+            Label label =new Label("empty list");
+            label.setAlignment(Pos.CENTER);
+            label.setFont(Font.font("System", FontWeight.BOLD,15));
+            BorderPane borderPane =new BorderPane();
+            borderPane.setCenter(label);
+            showNewPopUp(borderPane);
+        }
+        else
+            showNewPopUp(createListView(data));
+    }
+
+
+    private ListView<String> createListView(List<Object> list)
+    {
+        ListView<String> listView=new ListView<>();
+        listView.setOrientation(Orientation.VERTICAL);
+        int counter=1;
+        for(Object object:list)
+        {
+            listView.getItems().add(counter+"."+object.toString());
+            counter++;
+        }
+        return  listView;
+    }
+
+
+
+    private void addStepLogs(List<String> logs) {
+        if (logs.size() == 0)
+            addKeyValueLine( "","The step had no logs\n\n");
+        else {
+            for (String currLog : logs) {
+                addKeyValueLine( "",currLog+"\n\n");
+            }
+        }
+    }
+
+
+    public void updateFlowInfoView()
+    {
+        elementDetailsView.getChildren().clear();
+        if(flowExecutionDTO==null)
+            return;
+
+        updateFlowNameIDAndState();
+        if(flowExecutionDTO.getStateAfterRun()!=null)
+            addKeyValueLine("Flow total run time: " , flowExecutionDTO.getRunTime() + " ms");
+        else
+            addKeyProgressIndicator("Flow total run time: ");
+            //addKeyValueLine("Flow total run time: " ,  "flow is still running");
+
+
+        addTitleLine("\n\nFREE INPUTS THAT RECEIVED DATA:\n");
+        if(flowExecutionDTO.getFreeInputs().size()!=0) {
+            updateFlowFreeInputs(flowExecutionDTO.getFreeInputs(), true);
+            updateFlowFreeInputs(flowExecutionDTO.getFreeInputs(), false);
+        }
+        else
+            addKeyValueLine("","NO FREE INPUTS HAVE RECEIVED DATA");
+        addTitleLine("\nDATA PRODUCED (OUTPUTS):\n");
+        if(flowExecutionDTO.getOutputs().size()!=0)
+            updateOutputsHistoryData(flowExecutionDTO.getOutputs());
+        else
+            addKeyValueLine("","NO DATA WAS PRODUCED");
+        addTitleLine("\nFLOW STEPS DATA:\n");
+        updateStepsHistoryData(flowExecutionDTO.getSteps());
+
+    }
+
+    private void updateFlowNameIDAndState() {
+        addTitleLine("FLOW EXECUTION DATA:\n");
+        addKeyValueLine("Flows unique ID: ",flowExecutionDTO.getId());
+        addKeyValueLine("Flow name: ",flowExecutionDTO.getName());
+        if(flowExecutionDTO.getStateAfterRun()!=null)
+            addKeyValueLine("Flow's final state : " , flowExecutionDTO.getStateAfterRun());
+        else
+            addKeyProgressIndicator("Flow's final state :   ");
+            //addKeyValueLine("Flow's final state : " , "flow is still running");
+
+
+    }
+    private void updateFlowFreeInputs(List<FreeInputExecutionDTO> flowFreeInputs, boolean mandatoryOrNot)
+    {
+        for (FreeInputExecutionDTO freeInput : flowFreeInputs) {
+            if (freeInput.getData() != null) {
+                if(freeInput.isMandatory()==mandatoryOrNot) {
+
+                    addKeyValueLine("Name: ", freeInput.getName());
+                    addKeyValueLine("Type: ", freeInput.getType());
+                    addKeyValueLine("Input data: ", freeInput.getData());
+
+                    if (mandatoryOrNot)
+                        addKeyValueLine("This input is mandatory: ", "Yes\n\n");
+                    else
+                        addKeyValueLine("This input is mandatory: ", "No\n\n");
+                }
+            }
+        }
+
+    }
+
+    private void updateOutputsHistoryData(List<OutputExecutionDTO> outputs) {
+        for (OutputExecutionDTO output : outputs) {
+            addKeyValueLine("Name: " , output.getName());
+            addKeyValueLine("Type: " , output.getType());
+            if (output.getData() != null) {
+                if(output.getType().equals("List") || output.getType().equals("Relation")) {
+                    addKeyHyperLinkValueLine("Data",output.getType(),output.getData());
+                    addKeyValueLine("" ,"\n");
+                }
+                else
+                    addKeyValueLine("Data: " ,output.getData().toString()+"\n\n");
+
+            }
+            else
+                addKeyValueLine("Data: " ,"Not created due to failure in flow\n\n");
+        }
+    }
+
+    private void updateStepsHistoryData(List<StepExecutionDTO> steps) {
+        for (StepExecutionDTO step: steps) {
+            addKeyValueLine("Step Name: " , step.getName());
+            addKeyValueLine("Run time: " , step.getRunTime() + " ms");
+            addKeyValueLine("Finish state: " , step.getStateAfterRun());
+            addKeyValueLine("Step summary:" , step.getSummaryLine());
+            addKeyValueLine("\nSTEP LOGS:","");
+            addStepLogs(step.getStepExtensionDTO().getLogs());
+        }
+    }
+
+    public String getID() {
+        return flowExecutionDTO.getId();
+    }
+
+}
