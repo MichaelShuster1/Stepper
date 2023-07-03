d3216c579b446d713826a774513b148edb1ae385
Michael Shuster <michaelsu@mta.ac.il>
2023-07-03 16:25:12 +0300

    add Role object

---
+++ Engine/src/roles/Role.java
@@ -0,0 +1,32 @@
+package roles;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Role {
+    private final String name;
+    private final String description;
+    private Set<String> flowsAssigned;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.flowsAssigned=new HashSet<>();
+    }
+
+    public void addFlow(String flowName){
+        flowsAssigned.add(flowName);
+    }
+
+    public void removeFlow(String flowName){
+        flowsAssigned.remove(flowName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
