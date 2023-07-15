7603870b8bb2519c8bc72cf106a06503473c6d45
Michael Shuster <michaelsu@mta.ac.il>
2023-07-15 12:14:37 +0300

    added little fixes to ProgressTracker and UpdatesRefresher.

--- UserApp/src/controllers/flowexecution/ExecutionController.java
+++ UserApp/src/controllers/flowexecution/ExecutionController.java
@@ -609,7 +609,7 @@ public class ExecutionController {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 if(response.code()==200&&response.body()!=null){
                     String flowId=response.body().string();
-                    appController.addFlowId(flowId);
+                    appController.setFlowId(flowId);
                 }
                 else
                     HttpClientUtil.errorMessage(response.body(), appController);
--- UserApp/src/progress/ProgressTracker.java
+++ UserApp/src/progress/ProgressTracker.java
@@ -50,43 +50,45 @@ public class ProgressTracker extends Task<Boolean> {
 
             synchronized (currentFlowId) {
 
-                String finalUrl = HttpUrl
-                        .parse(Constants.FULL_SERVER_PATH + "/get-progress")
-                        .newBuilder()
-                        .addQueryParameter("flowId", currentFlowId)
-                        .build()
-                        .toString();
+                if(currentFlowId!=null){
+                    String finalUrl = HttpUrl
+                            .parse(Constants.FULL_SERVER_PATH + "/get-progress")
+                            .newBuilder()
+                            .addQueryParameter("flowId", currentFlowId)
+                            .build()
+                            .toString();
 
 
-                Response response = HttpClientUtil.runSync(finalUrl);
+                    Response response = HttpClientUtil.runSync(finalUrl);
 
-                if (response == null) {
-                    System.out.println("problem with fetching flow progress");
-                } else {
-                    if (response.code() == 200 && response.body() != null) {
-                        try {
-                            FlowExecutionDTO flowExecutionDTO = Constants.GSON_INSTANCE
-                                    .fromJson(response.body().string(), FlowExecutionDTO.class);
-
-                            Platform.runLater(() -> appController.updateProgressFlow(flowExecutionDTO));
-
-                        } catch (Exception e) {
-                            System.out.println(e.getMessage());
-                        }
-                    } else
+                    if (response == null) {
                         System.out.println("problem with fetching flow progress");
+                    } else {
+                        if (response.code() == 200 && response.body() != null) {
+                            try {
+                                FlowExecutionDTO flowExecutionDTO = Constants.GSON_INSTANCE
+                                        .fromJson(response.body().string(), FlowExecutionDTO.class);
 
-                    if (response.body() != null)
-                        response.body().close();
+                                Platform.runLater(() -> appController.updateProgressFlow(flowExecutionDTO));
+
+                                if(flowExecutionDTO.getStateAfterRun()!=null)
+                                    currentFlowId=null;
+
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+                        } else
+                            System.out.println("problem with fetching flow progress");
+
+                        if (response.body() != null)
+                            response.body().close();
+                    }
                 }
-
             }
-
             try {
                 Thread.sleep(700);
             }
             catch (InterruptedException e) {}
-
         }
         return true;
     }
