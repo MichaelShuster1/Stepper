247d5aa3d60f42babca35b86ff7aaee3a1029ec6
Michael Shuster <michaelsu@mta.ac.il>
2023-07-17 20:56:27 +0300

    added little things in chat

--- UserApp/src/controllers/chat/ChatAreaController.java
+++ UserApp/src/controllers/chat/ChatAreaController.java
@@ -2,14 +2,12 @@ package controllers.chat;
 
 import controllers.chat.model.ChatLinesWithVersion;
 import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleButton;
 import okhttp3.Call;
@@ -20,7 +18,6 @@ import org.jetbrains.annotations.NotNull;
 import utils.Constants;
 import utils.HttpClientUtil;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.stream.Collectors;
@@ -29,34 +26,30 @@ import static utils.Constants.CHAT_LINE_FORMATTING;
 import static utils.Constants.REFRESH_RATE;
 
 
-public class ChatAreaController implements Closeable {
+public class ChatAreaController {
+
+    @FXML
+    private ToggleButton autoScrollButton;
+    @FXML
+    private TextArea chatLineTextArea;
+    @FXML
+    private TextArea mainChatLinesTextArea;
 
     private final IntegerProperty chatVersion;
     private final BooleanProperty autoScroll;
-    private final BooleanProperty autoUpdate;
     private ChatAreaRefresher chatAreaRefresher;
     private Timer timer;
 
-    @FXML private ToggleButton autoScrollButton;
-    @FXML private TextArea chatLineTextArea;
-    @FXML private TextArea mainChatLinesTextArea;
-    @FXML private Label chatVersionLabel;
 
     public ChatAreaController() {
         chatVersion = new SimpleIntegerProperty();
         autoScroll = new SimpleBooleanProperty();
-        autoUpdate = new SimpleBooleanProperty();
     }
 
     @FXML
     public void initialize() {
         autoScroll.bind(autoScrollButton.selectedProperty());
-        chatVersionLabel.textProperty().bind(Bindings.concat("Chat Version: ", chatVersion.asString()));
         startListRefresher();
-    }
-
-    public BooleanProperty autoUpdatesProperty() {
-        return autoUpdate;
     }
 
     @FXML
@@ -78,12 +71,10 @@ public class ChatAreaController implements Closeable {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.code()==200){
-
+                if(response.code()!=200){
+                    System.out.println("there was a problem with sending the message");
                 }
-                else {
 
-                }
                 if(response.body()!=null)
                     response.body().close();
             }
@@ -122,18 +113,13 @@ public class ChatAreaController implements Closeable {
     public void startListRefresher() {
         chatAreaRefresher = new ChatAreaRefresher(
                 chatVersion,
-                autoUpdate,
                 this::updateChatLines);
         timer = new Timer();
-        timer.schedule(chatAreaRefresher, REFRESH_RATE, REFRESH_RATE);
+        timer.schedule(chatAreaRefresher, 0, REFRESH_RATE);
     }
 
-    public void stopListRefresher(){
-        timer.cancel();
-    }
 
-    @Override
-    public void close() throws IOException {
+    public void close()  {
         chatVersion.set(0);
         chatLineTextArea.clear();
         if (chatAreaRefresher != null && timer != null) {
